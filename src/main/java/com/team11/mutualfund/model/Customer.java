a53d4991acde2a1ba7aa0d032dbbe9dbcf6341a5
gy <marcusgao@163.com>
2017-01-19 12:19:48 -0500

    add customer

--- src/main/java/com/team11/mutualfund/model/Customer.java
+++ src/main/java/com/team11/mutualfund/model/Customer.java
@@ -1,24 +1,11 @@
 package com.team11.mutualfund.model;
 
-import java.math.BigDecimal;
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.Table;
-import javax.validation.constraints.Digits;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
-
-import org.hibernate.annotations.Type;
-import org.hibernate.validator.constraints.NotEmpty;
-import org.joda.time.LocalDate;
-import org.springframework.format.annotation.DateTimeFormat;
 
 @Entity
-public class Employee {
+public class Customer {
 
 	@Id
 	private String userName;
