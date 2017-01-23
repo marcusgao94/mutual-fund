9f5cba5481aadd382cfc00023efdac961e47fc0d
YLiu78 <YLiu78@10.232.15.179>
2017-01-22 19:56:09 -0500

    add fundService

--- src/main/java/com/team11/mutualfund/service/FundService.java
+++ src/main/java/com/team11/mutualfund/service/FundService.java
@@ -1,12 +1,24 @@
 package com.team11.mutualfund.service;
 
 import com.team11.mutualfund.dao.FundDao;
+import com.team11.mutualfund.model.Fund;
+import com.team11.mutualfund.model.Customer;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@Transactional
 public class FundService {
     @Autowired
     private FundDao fundDao;
 
+    public boolean createFund(Fund fund, Customer customer) {
+		if (fundDao.listFundByCustomerId(customer.getId()) != null) {
+			return false;
+		}
+		fundDao.saveFund(fund,customer.getId());
+		return true;
+	}
 }
