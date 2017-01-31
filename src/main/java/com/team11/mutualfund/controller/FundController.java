0795ec1d74a3bfe03b92a0b74da20fcd6b0feee6
Nanxi Li <nanxil@andrew.cmu.edu>
2017-01-31 15:37:12 -0500

    update the name of cv variable

--- src/main/java/com/team11/mutualfund/controller/FundController.java
+++ src/main/java/com/team11/mutualfund/controller/FundController.java
@@ -69,7 +69,7 @@ public class FundController {
         User user = (User) request.getSession().getAttribute("user");
         Customer c = customerService.getCustomerByUserName(user.getUserName());
         List<Positionvalue> pv = fundService.listPositionvalueByCustomerId(c.getId());
-        model.addAttribute("customerPositione", pv);
+        model.addAttribute("customerPosition", pv);
         try {
             transactionService.buyFund(user.getId(), buyFundForm.getFundTicker(),
                 buyFundForm.getAmount());
@@ -121,7 +121,7 @@ public class FundController {
         User user = (User) request.getSession().getAttribute("user");
         Customer c = customerService.getCustomerByUserName(user.getUserName());
         List<Positionvalue> pv = fundService.listPositionvalueByCustomerId(c.getId());
-        model.addAttribute("customerPositione", pv);
+        model.addAttribute("customerPosition", pv);
         try {
             transactionService.sellFund(user.getId(), sellFundForm.getFundTicker(), sellFundForm.getShare());
         } catch (RollbackException e) {
